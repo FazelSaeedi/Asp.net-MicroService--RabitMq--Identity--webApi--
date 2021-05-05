178cd2c70dbefa1c254e8b0199821c2a5848a115
Fazel Saeedi <fazels36@gmail.com>
2021-05-06 01:31:20 +0430

    install package && Create Hostbuilder && BusBuilder Class

--- src/Microservice.Common/Services/ServiceHost.cs
+++ src/Microservice.Common/Services/ServiceHost.cs
@@ -1,7 +1,11 @@
 using System;
+using Microservice.Common.Commands;
+using Microservice.Common.Events;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using RawRabbit;
+using src.Microservice.Common.Events;
 
 namespace src.Microservice.Common.Services
 {
@@ -31,6 +35,74 @@ namespace src.Microservice.Common.Services
             .UseStartup<TSturtup>();
 
             return new HostBuilder(webHostBuilder.Build());
+        }
+
+        public abstract class BuilderBase
+        {
+            public abstract ServiceHost Build();
+        }
+
+        public class HostBuilder : BuilderBase
+        {
+            private  IBusClient _bus ;
+            private readonly IWebHost _webHost;
+
+            public HostBuilder(IWebHost webHost)
+            {
+                _webHost = webHost;
+            }
+
+            public BusBuilder UseRabbitMq()
+            {
+                _bus = (IBusClient) _webHost.Services.GetService(typeof(IBusClient));
+
+                return new BusBuilder(_webHost , _bus );
+            }
+
+            public override ServiceHost Build()
+            {
+                return new ServiceHost(_webHost);
+            }
+            
+        }
+
+        public class BusBuilder : BuilderBase
+        {
+
+            private  IBusClient _bus ;
+            private readonly IWebHost _webHost;
+
+            public BusBuilder(IWebHost webHost , IBusClient busClient)
+            {
+                _webHost = webHost;
+                _bus = busClient ;
+            }
+
+
+            public BusBuilder SubscribeToCommand<TCommand>() where TCommand : ICommand
+            {
+                var handler = (ICommandHandler<TCommand>) _webHost.Services
+                .GetService(typeof(ICommandHandler<TCommand>));
+
+                _bus.WithCommandHandlerAsync(handler);
+
+                return this;
+            }
+
+            public BusBuilder SubscribeToEvent<TEvent>() where TEvent : IEvent
+            {
+                 var handler = (IEventHandler<TEvent>) _webHost.Services
+                 .GetService(typeof(IEventHandler<TEvent>));
+
+                _bus.WithEventHandlerAsync(handler);
+
+                return this;
+            }
+
+            public override ServiceHost Build()
+            {
+                return new ServiceHost(_webHost);
+            }
         }
     }
 }
