d942ec6f8b65b53f95b380feae940f553f77c3f8
Fazel Saeedi <fazels36@gmail.com>
2021-05-06 05:41:24 +0430

    Create Models and Repositories in Activities Service

---
+++ src/Microservice.Services.Activities/Domain/Models/Activity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.Services.Activities.Domain.Models
+{
+    public class Activity
+    {
+        public Guid Id { get; protected set; }
+
+
+        public string Category { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public string Name { get; protected set; }
+        public String Description { get; protected set; }
+        public DateTime CreateAt  { get; protected set; }
+        
+        protected Activity()
+        {
+
+        }
+
+
+        public Activity(Guid id, string category, Guid userId, string name , string description, DateTime createAt)
+        {
+
+            if(string.IsNullOrWhiteSpace(name))
+                throw new Exception();
+
+
+
+            Id = id;
+            Category = category;
+            UserId = userId;
+            Name = name;
+            Description = description;
+            CreateAt = createAt;
+
+        }
+
+
+    }
+}
---
+++ src/Microservice.Services.Activities/Domain/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice.Services.Activities.Domain.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        protected Category()
+        {
+
+        }
+
+        public Category(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name.ToLowerInvariant();
+        }
+    }
+}
