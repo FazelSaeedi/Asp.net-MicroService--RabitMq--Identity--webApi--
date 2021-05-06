169456790cc601932bd1470efd8c7122b833178c
Fazel Saeedi <fazels36@gmail.com>
2021-05-06 05:59:03 +0430

    install mongodb Package

--- src/Microservice.Services.Activities/Domain/Repositoies/ICategoryRepository.cs
+++ src/Microservice.Services.Activities/Domain/Repositoies/ICategoryRepository.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microservice.Services.Activities.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Microservice.Services.Activities.Domain.Repositoies
 {
-    interface ICategoryRepository
+    public interface ICategoryRepository
     {
+        Task<Category> GetAsync(string name);
+        Task<IEnumerable<Category>> BrowseAsync();
+        Task AddAsync(Category category); 
     }
 }
